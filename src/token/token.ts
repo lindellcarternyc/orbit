d1ed44046f7dd6a7ca19527b16fe3e5b7a46aa55
lindellcarternyc <lindellcarternyc@gmail.com>
2022-01-16 15:35:08 -0500

    creates token module

---
+++ src/token/token.ts
@@ -0,0 +1,79 @@
+import { LPosition } from '../l-position'
+
+export enum TokenType {
+  Keyword = 'KEYWORD',
+  Identifier = 'IDENTIFIER',
+
+  // Punctuation
+  LParens = 'LEFT PARENS',
+  RParens = 'RIGHT PARENS',
+
+  LBrace = 'LEFT BRACE',
+  RBrace = 'RIGHT BRACE',
+
+  LBracket = 'LEFT BRACKET',
+  RBracket = 'RIGHT BRACKET',
+  Punctuation = 'PUNCTUATION',
+
+  // Values
+  Bool = 'BOOLEAN',
+  Number = 'NUMBER',
+  String = 'STRING',
+
+  // Operators
+  BinaryOperator = 'BINARY OPERATOR',
+  AssignmentOperator = 'ASSIGNMENT OPERATOR',
+  DotOperator = 'DOT OPERATOR',
+
+  // MISC
+  Whitespace = 'WHITESPACE',
+  Newline = 'NEWLINE',
+  Unrecognized = 'UNRECOGNIZED'
+}
+
+export interface LToken<T extends TokenType, V extends string = string> {
+  type: T
+  value: V
+  range: {
+    start: LPosition
+    end: LPosition
+  }
+}
+
+export type KeywordToken = LToken<TokenType.Keyword>
+export type IdentifierToken = LToken<TokenType.Identifier>
+export type UnrecognizedToken = LToken<TokenType.Unrecognized>
+export type ParenToken = 
+  | LToken<TokenType.LParens, '('>
+  | LToken<TokenType.RParens, ')'>
+export type BraceToken = 
+  | LToken<TokenType.LBrace, '['>
+  | LToken<TokenType.RBrace, ']'>
+export type BracketToken =
+  | LToken<TokenType.LBracket, '{'>
+  | LToken<TokenType.RBracket, '}'>
+export type PuncToken = LToken<TokenType.Punctuation>
+export type BoolToken = LToken<TokenType.Bool, 'true' | 'false'>
+export type NumberToken = LToken<TokenType.Number>
+export type StringToken = LToken<TokenType.String>
+export type OperatorToken = 
+  | LToken<TokenType.BinaryOperator>
+  | LToken<TokenType.AssignmentOperator, '='>
+  | LToken<TokenType.DotOperator, '.'>
+export type WhitespaceToken = LToken<TokenType.Whitespace>
+export type NewlineToken = LToken<TokenType.Newline>
+
+export type Token = 
+  | KeywordToken
+  | IdentifierToken
+  | BoolToken
+  | ParenToken
+  | BraceToken
+  | BracketToken
+  | PuncToken
+  | NumberToken
+  | StringToken
+  | OperatorToken
+  | WhitespaceToken
+  | NewlineToken
+  | UnrecognizedToken
